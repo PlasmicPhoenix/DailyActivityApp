a4a0526862a7b4e86c9fc162ca77d68645000b18
PlasmicPhoenix <captainslash502@gmail.com>
2024-01-06 14:31:44 -0700

    Add project files.

---
+++ Main Window.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DailyActivityApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.title = new System.Windows.Forms.Label();
+            this.snoozeBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // title
+            // 
+            this.title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Georgia", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.title.Location = new System.Drawing.Point(3, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(263, 31);
+            this.title.TabIndex = 0;
+            this.title.Text = "Daily Activity App";
+            this.title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // snoozeBox
+            // 
+            this.snoozeBox.AutoSize = true;
+            this.snoozeBox.BackColor = System.Drawing.Color.Transparent;
+            this.snoozeBox.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.snoozeBox.Location = new System.Drawing.Point(12, 43);
+            this.snoozeBox.Name = "snoozeBox";
+            this.snoozeBox.Size = new System.Drawing.Size(99, 27);
+            this.snoozeBox.TabIndex = 1;
+            this.snoozeBox.Text = "Snooze";
+            this.snoozeBox.UseVisualStyleBackColor = false;
+            this.snoozeBox.CheckedChanged += new System.EventHandler(this.snoozeBox_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(12, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Random Level One";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.ButtonOne_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Location = new System.Drawing.Point(12, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(241, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Random Level Two";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button3.Location = new System.Drawing.Point(12, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(241, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Random Level Three";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(265, 197);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.snoozeBox);
+            this.Controls.Add(this.title);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Daily Activity App";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.CheckBox snoozeBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Main Window.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Runtime.Remoting.Channels;
+
+namespace DailyActivityApp
+{
+    public partial class Main : Form
+    {
+        Random random = new Random();
+        string[] levelOne;
+        string[] levelTwo;
+        string[] levelThree;
+
+        string[] instructionsOne;
+        string[] instructionsTwo;
+        string[] instructionsThree;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            AddData();
+
+            ToastNotificationManagerCompat.OnActivated += HandleToastActivation;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ExerciseTick();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void snoozeBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (snoozeBox.Checked)
+            {
+                MessageBox.Show("You better not have this snoozed for too long!", "Are you sure?");
+                return;
+            }
+
+            ExerciseTick();
+        }
+
+        private void ButtonOne_Click(object sender, EventArgs e)
+        {
+            ForwardExercise(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ForwardExercise(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ForwardExercise(3);
+        }
+
+        private void HandleToastActivation(ToastNotificationActivatedEventArgsCompat args)
+        {
+            ToastArguments toastArgs = ToastArguments.Parse(args.Argument);
+            string stringLevel;
+            string stringExercise;
+            toastArgs.TryGetValue("Level", out stringLevel);
+            toastArgs.TryGetValue("Exercise", out stringExercise);
+
+            int level = int.Parse(stringLevel);
+            int exercise = int.Parse(stringExercise);
+
+            switch (level)
+            {
+                case 1:
+                    Process.Start(instructionsOne[exercise]);
+                    break;
+                case 2:
+                    Process.Start(instructionsTwo[exercise]);
+                    break;
+                case 3:
+                    Process.Start(instructionsThree[exercise]);
+                    break;
+            }
+        }
+
+        //Helpers
+        private void AddData()
+        {
+            StreamReader reader = new StreamReader(@"..\..\Exercise Data\LevelOne.txt");
+            levelOne = reader.ReadToEnd().Split('\n');
+
+            reader = new StreamReader(@"..\..\Exercise Data\OneURL.txt");
+            instructionsOne = reader.ReadToEnd().Split('\n');
+            for (int i = 0; i < instructionsOne.Length; i++)
+            {
+                instructionsOne[i] = instructionsOne[i].Replace("\n", "");
+            }
+
+            reader = new StreamReader(@"..\..\Exercise Data\LevelTwo.txt");
+            levelTwo = reader.ReadToEnd().Split('\n');
+
+            reader = new StreamReader(@"..\..\Exercise Data\TwoURL.txt");
+            instructionsTwo = reader.ReadToEnd().Split('\n');
+            for (int i = 0; i < instructionsTwo.Length; i++)
+            {
+                instructionsTwo[i] = instructionsTwo[i].Replace("\n", "");
+            }
+
+            reader = new StreamReader(@"..\..\Exercise Data\LevelThree.txt");
+            levelThree = reader.ReadToEnd().Split('\n');
+
+            reader = new StreamReader(@"..\..\Exercise Data\ThreeURL.txt");
+            instructionsThree = reader.ReadToEnd().Split('\n');
+            for (int i = 0; i < instructionsThree.Length; i++)
+            {
+                instructionsThree[i] = instructionsThree[i].Replace("\n", "");
+            }
+        }
+
+        private void ExerciseTick()
+        {
+            timer.Interval = random.Next(1800000, 3600000);
+
+            if (snoozeBox.Checked)
+            {
+                return;
+            }
+
+            int randomResult;
+
+            if (DateTime.Now.TimeOfDay.TotalHours > 18 || DateTime.Now.TimeOfDay.TotalHours < 8)
+            {
+                randomResult = random.Next(1, 9);
+            }
+            else
+            {
+                randomResult = random.Next(1, 11);
+            }
+
+            switch (randomResult)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    ForwardExercise(1);
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    ForwardExercise(2);
+                    break;
+                case 9:
+                case 10:
+                    ForwardExercise(3);
+                    break;
+            }
+        }
+
+        private void ForwardExercise(int level)
+        {
+            int randomResult;
+
+            switch (level)
+            {
+                case 1:
+                    randomResult = random.Next(0, levelOne.Length);
+                    new ToastContentBuilder()
+                        .AddText("Time for a level 1 exercise!")
+                        .AddText("Do " + (random.Next(1, 11) * 5) + " " + levelOne[randomResult])
+                        .AddButton(new ToastButton().SetContent("Show Activity").AddArgument("Level", 1).AddArgument("Exercise", randomResult))
+                        .Show();
+                    break;
+
+                case 2:
+                    randomResult = random.Next(0, levelTwo.Length);
+                    new ToastContentBuilder()
+                        .AddText("Time for a level 2 exercise!")
+                        .AddText("Do " + (random.Next(1, 5) * 5) + " " + levelTwo[randomResult])
+                        .AddButton(new ToastButton().SetContent("Show Activity").AddArgument("Level", 2).AddArgument("Exercise", randomResult))
+                        .Show();
+                    break;
+
+                case 3:
+                    randomResult = random.Next(0, levelThree.Length);
+                    new ToastContentBuilder()
+                        .AddText("Time for a level 3 exercise!")
+                        .AddText(levelThree[randomResult])
+                        .AddButton(new ToastButton().SetContent("Show Activity").AddArgument("Level", 3).AddArgument("Exercise", randomResult))
+                        .Show();
+                    break;
+            }
+        }
+    }
+}
